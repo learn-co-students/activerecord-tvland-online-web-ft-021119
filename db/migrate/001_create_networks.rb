703b0305a04b54f73cb6b0e669d30221491c6dd7
github username <github email address>
2019-03-25 19:09:09 -0400

    Done.

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -3,7 +3,7 @@
 #by convention, the class name should match the part of the
 #file name after the number, so in this case:
 #001_create_networks.rb becomes class CreateNetworks
-class CreateNetworks < ActiveRecord::Migration
+class CreateNetworks < ActiveRecord::Migration[4.2]
   #define a change method in which to do the migration
   def change
     create_table :networks do |t| #we get a block variable here for the table
@@ -13,6 +13,7 @@ class CreateNetworks < ActiveRecord::Migration
       # the above breaks down to
       # "create a column called :call_letters on table t with type string
       t.integer :channel
+      t.string :shows
     end
   end
 end
