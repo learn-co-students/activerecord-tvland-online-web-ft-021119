132d55974d36b8a35cf9c21340e339c94bc11965
lhsharing <daley.luis@gmail.com>
2019-03-26 19:00:57 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters# belongs_to :show
+
+    def roles
+        self
+        #characters.map do |thesbian|
+        #"#{thesbian.name} - #{thesbian.show.name}"
+        end
 end
