7fd7c192c3c534c2c543bc7a9eb43279b91f09e4
BebopVinh <vinhdo07@gmail.com>
2019-03-26 19:18:12 -0700

    working on character chain-build

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
-end
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def list_roles
+    roles = self.characters.map do |character|
+      character.name + " - " + character.show.name
+    end
+  end
+
+end #End of Actor Class
