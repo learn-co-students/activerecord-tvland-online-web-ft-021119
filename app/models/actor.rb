fb2922a66f5617c3fb6e94bd3a73bf47a727d077
bbennett7 <bryn.bennett@gmail.com>
2019-03-28 10:52:07 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,14 +6,6 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
-  #def list_roles
-  #  self.characters.collect do |character|
-  #    if Show.first.id == Character.first.show.id
-  #      "#{Character.first.name} - #{Show.first.name}"
-  #    end
-  #  end
-  #end
-
   def list_roles
     self.characters.collect do |character|
     "#{character.name} - #{character.show.name}"
